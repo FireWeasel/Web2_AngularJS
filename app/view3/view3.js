22c7109e3ff3af20abd54bc6c2ab04610520ffb7
LuboYankov <ahhak123@gmail.com>
2017-09-11 22:27:21 +0200

    Adding create task functionality

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -14,17 +14,29 @@ angular.module('myApp.view3', ['ngRoute'])
         $scope.tasks = [
             {"number":"1",
              "description":"Clean the kitchen",
-             "completed":"Yes"        
+             "completed":"Yes"
             }, {"number":"2",
              "description":"Clean the living room",
-             "completed":"No"        
+             "completed":"No"
             }, {"number":"3",
              "description":"Clean the room",
-             "completed":"No"        
+             "completed":"No"
             }, {"number":"4",
              "description":"Do the WEB2 homework",
-             "completed":"Yes"        
+             "completed":"Yes"
             }
         ];
+
+        $scope.AddTaskToList = function(){
+
+              if ($scope.inputNumber && $scope.inputDescription && $scope.inputCompleted){
+                  $scope.tasks.push({
+                          "number": $scope.inputNumber,
+                          "description": $scope.inputDescription,
+                          "completed": $scope.inputCompleted
+                  });
+              }
+
+          };
 
 }]);
