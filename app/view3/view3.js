dcb9968626c1ba1632c813ef897570d4dc882121
LuboYankov <ahhak123@gmail.com>
2017-09-19 01:38:14 +0200

    Adding task detailed view and updating .gitignore

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -11,52 +11,60 @@ angular.module('myApp.view3', ['ngRoute'])
 
 .controller('View3Ctrl', [ '$scope', function($scope) {
 
-        $scope.tasks = [
-            {"number":"1",
-             "description":"Clean the kitchen",
-             "completed":"Yes"
-            }, {"number":"2",
-             "description":"Clean the living room",
-             "completed":"No"
-            }, {"number":"3",
-             "description":"Clean the room",
-             "completed":"No"
-            }, {"number":"4",
-             "description":"Do the WEB2 homework",
-             "completed":"Yes"
-            }
-        ];
+  $scope.IsVisible = false;
 
-        $scope.AddTaskToList = function(){
+  $scope.tasks = [
+      {"number":"1",
+       "description":"Clean the kitchen",
+       "completed":"Yes"
+      }, {"number":"2",
+       "description":"Clean the living room",
+       "completed":"No"
+      }, {"number":"3",
+       "description":"Clean the room",
+       "completed":"No"
+      }, {"number":"4",
+       "description":"Do the WEB2 homework",
+       "completed":"Yes"
+      }
+  ];
 
-          if ($scope.inputNumber && $scope.inputDescription && $scope.inputCompleted){
-              $scope.tasks.push({
-                      "number": $scope.inputNumber,
-                      "description": $scope.inputDescription,
-                      "completed": $scope.inputCompleted
-              });
-          }
+  $scope.AddTaskToList = function(){
 
-        };
+    if ($scope.inputNumber && $scope.inputDescription && $scope.inputCompleted){
+        $scope.tasks.push({
+                "number": $scope.inputNumber,
+                "description": $scope.inputDescription,
+                "completed": $scope.inputCompleted
+        });
+    }
 
-        $scope.ComboBoxRemove = function() {
-           var index = $scope.selectedTaskIndex;
+  };
 
-           if (index > -1) {
-             $scope.tasks.splice(index, 1);
-           }
-        };
+  $scope.ComboBoxRemove = function() {
+     var index = $scope.selectedTaskIndex;
 
-        $scope.UpdateTask = function(){
+     if (index > -1) {
+       $scope.tasks.splice(index, 1);
+     }
+  };
 
-          var index = parseInt($scope.selectedTaskIndexForUpdate) + 1;
+  $scope.UpdateTask = function(){
 
-          function checkNumber(task) {
-            return task.number == index;
-          }
+    var index = parseInt($scope.selectedTaskIndexForUpdate) + 1;
 
-          $scope.tasks.find(checkNumber).description = $scope.updatedInputDescription;
-          $scope.tasks.find(checkNumber).completed = $scope.updatedInputCompleted;
-        };
+    function checkNumber(task) {
+      return task.number == index;
+    }
+
+    $scope.tasks.find(checkNumber).description = $scope.updatedInputDescription;
+    $scope.tasks.find(checkNumber).completed = $scope.updatedInputCompleted;
+  };
+
+  $scope.ShowInfo = function(task) {
+    var myEl = angular.element(document.querySelector('#divData'));
+    $scope.IsVisible = true;
+    myEl.html("Completed: " + task.completed);
+  };
 
 }]);
