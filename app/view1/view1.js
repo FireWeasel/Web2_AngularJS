1da85998c8ef17ce69df68b36541172da2fc27e0
Marina Tzenkova <m.tsenkova@student.fontys.nl>
2017-09-18 16:00:01 +0200

    Fixing code

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,36 +12,53 @@ angular.module('myApp.view1', ['ngRoute'])
 .controller('View1Ctrl', [ '$scope', function($scope) {
     $scope.departments = [
             {   
+            	"id" : "1",
                 "Name":"Management",
                 "Headquarters":"New York City"
             },
             {
+            	"id" : "2",
                 "Name":"Sales",
                 "Headquarters":"Chicago",
             },
             {
+            	"id" : "3",
                 "Name":"Marketing",
                 "Headquarters":"Boston"
             },
             {
+            	"id" : "4", 
                 "Name":"IT",
                 "Headquarters":"Seattle"
             }
         ];
         
         $scope.AddDepartmentToList = function() {
-        if($scope.inputName && $scope.inputHeadquarters) {
+        if($scope.inputName && $scope.inputHeadquarters && $scope.inputId) {
         $scope.departments.push({
+        				"id" : $scope.inputId,
                         "Name": $scope.inputName,
                         "Headquarters": $scope.inputHeadquarters
                         })};
         }
         
         $scope.CBRemove = function() {
-           var index = $scope.selectedEmployeeIndex;
+           var index = $scope.selectedDepartmentIndex;
            
             if (index > -1) {
                $scope.departments.splice(index, 1);
             }
+        }
+        
+        $scope.UpdateDepartment = function(){
+
+          var index = parseInt($scope.SelectedDepartmentUpdate) + 1;
+
+          function checkNumber(department) {
+            return department.id == index;
+          }
+		  $scope.departments.find(checkNumber).id = $scope.updateId;
+          $scope.departments.find(checkNumber).Name = $scope.updatedInputName;
+          $scope.departments.find(checkNumber).Headquarters = $scope.updatedInputHeadquarters;
         };
 }]);
