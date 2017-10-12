1204e2a84f43db6a72a2dbc62cd22ea1bad93347
Marina Tzenkova <m.tsenkova@student.fontys.nl>
2017-10-12 22:27:53 +0200

    Finishing departments view

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -8,23 +8,28 @@ var myApp1 = angular.module('myApp.view1', ['ui.bootstrap', 'ngRoute'])
         });
     }])
 //, 'myEmployees'               , myEmployees
-myApp1.controller('View1Ctrl', ['$scope', 'myDepartments','getDepartment','myEmployees', function($scope, myDepartments, getDepartment,myEmployees) {
+myApp1.controller('View1Ctrl', ['$scope', 'myDepartments','getDepartment','myEmployees','myTasks', function($scope, myDepartments, getDepartment,myEmployees,myTasks) {
 
 
 
     $scope.departments = myDepartments.data;
     $scope.employees = myEmployees.data;
+    $scope.tasks = myTasks.data;
     $scope.EmpOfDep = "none";
-    $scope.EmpOfDepNames = [];
     $scope.TaskOfDep = "none";
+    $scope.EmpOfDepNames = [];
+    $scope.TaskOfDepName = [];
 
     $scope.getEmployeeNames = function(depID)
     {
         //have to clear list because the function is called with every click of the button :)
         $scope.EmpOfDepNames = [];
+        $scope.TaskOfDepName = [];
+
         getDepartment.getHisDep(depID)
             .then(function(response){
                     $scope.EmpOfDep = response.data.employees;
+                    $scope.TaskOfDep = response.data.tasks;
 
                     for(var i = 0; i < $scope.EmpOfDep.length; i++)
                     {
@@ -36,6 +41,16 @@ myApp1.controller('View1Ctrl', ['$scope', 'myDepartments','getDepartment','myEmp
                         }
                       }
                     }
+                      for(var k = 0; k < $scope.TaskOfDep.length; k++)
+                      {
+                        for(var j = 0; j < $scope.tasks.length; j++)
+                        {
+                          if($scope.TaskOfDep[k] == $scope.tasks[j].no)
+                          {
+                            $scope.TaskOfDepName.push($scope.tasks[j].title);
+                          }
+                        }
+                      }
             },function(error){
                             alert("Error happened in getAnEmployee service calling:     " + error);
         });
@@ -49,16 +64,13 @@ myApp1.controller('View1Ctrl', ['$scope', 'myDepartments','getDepartment','myEmp
                 "code": $scope.inputCode
             });
         };
+        $scope.inputId = "";
+        $scope.inputName = "";
+        $scope.inputCode = "";
     };
 
-    function getAllEmployees (depID){
-
-
-
-    };
-
-    $scope.CBRemove = function() {
-        var index = $scope.selectedDepartmentIndex;
+    $scope.RemoveDepartment = function(department) {
+        var index = $scope.departments.indexOf(department);
 
         if (index > -1) {
             $scope.departments.splice(index, 1);
@@ -88,8 +100,10 @@ myApp1.directive('listEmps', function()
 {
   return {
     template: 'Employees: <ul>    \n\
-               <li ng-repeat="employee in EmpOfDepNames | limitTo: 10 "><b>   {{employee}}      </b></li>\n\
-            </ul>'
+               <li ng-repeat="employee in EmpOfDepNames | limitTo: 10 "><b> {{employee}} </b></li>\n\
+            </ul> \n\
+            Tasks: <ul> \n\
+              <li ng-repeat="tasks in TaskOfDepName"><b> {{tasks}} </b></li>'
   }
 });
 
@@ -118,54 +132,27 @@ myApp1.directive('departmentDetail', function(){
   }
 });
 
-myApp1.directive('taskModal', function(){
-  return {
-    template:'<!-- Modal --> \n\
-<div id="taskModal" class="modal fade" role="dialog"> \n\
-  <div class="modal-dialog"> \n\
-\n\
-    <!-- Modal content--> \n\
-    <div class="modal-content"> \n\
-      <div class="modal-header"> \n\
-        <button type="button" class="close" data-dismiss="modal">&times;</button> \n\
-        <h4 class="modal-title">Modal Header</h4> \n\
-      </div> \n\
-      <div class="modal-body"> \n\
-        <p></p> \n\
-      </div> \n\
-      <div class="modal-footer"> \n\
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> \n\
-      </div> \n\
-    </div> \n\
- \n\
-  </div> \n\
-</div>'
-  }
-});
-
 myApp1.directive('departmentsTable', function() {
     return {
         template: '<div ng-init="checked = true"> \n\
   <div class="content-area sample-show-hide"> \n\
     <table class="table table-bordered table-hover">\n\
                     <tr class="TableDep">\n\
-                        <th ng-show="checked">no</td>\n\
-                        <th ng-show="checked">code</td>\n\
-                        <th>name</td>\n\
+                        <th ng-show="checked">No</td>\n\
+                        <th ng-show="checked">Code</td>\n\
+                        <th>Name</td>\n\
                         <th ng-show="checked">Employees and Tasks</td>\n\
+                        <th>Remove</td> \n\
                     </tr>\n\
 					<tr ng-repeat="department in departments | filter: filterValue">\n\
                         <td ng-show="checked">{{department.no}}</td>\n\
                         <td ng-show="checked">{{department.code}}</td>\n\
                         <td>{{department.name}}</td>\n\
-                        <td ng-show="checked"><button class="btn btn-info" ng-click="getEmployeeNames(department.no)" data-toggle="modal" data-target="#departmentModal">Click me:</button></td>\n\
+                        <td ng-show="checked"><button class="btn btn-info" ng-click="getEmployeeNames(department.no)" data-toggle="modal" data-target="#departmentModal">Click me</button></td> \n\
+                        <td align="center"><a class="btn btn-danger" ng-click="RemoveDepartment(department)"><em class="fa fa-trash"></em></a></td> \n\
                     </tr>\n\
                 </table> \n\
   </div> \n\
 </div>'
-
-
-
-
     };
 });
